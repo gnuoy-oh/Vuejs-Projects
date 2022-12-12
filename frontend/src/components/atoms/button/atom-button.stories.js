9c7ad66e03cf91e86e9a952009579d57924663dd
gnuoy-oh <oh.my00418@gmail.com>
2022-12-12 11:39:25 +0900

    feat: 스토리북 - button 컴포넌트 추가, loadsh 추가

--- frontend/src/components/atoms/button/atom-button.stories.js
+++ frontend/src/components/atoms/button/atom-button.stories.js
@@ -1,40 +1,47 @@
 import AtomButton from "./atom-button.vue";
 
 export default {
-  title: "Atoms/Button",
-  components: AtomButton,
-  decorators: [
-    () => ({
-      template:
-          "<div class=\"storybook-container\"><h2 class=\"storybook-title\">다중 항목 선택 시 사용합니다.</h2><div class=\"storybook-group\"><story /></div></div>"
-    })
-  ]
+    title: "Atoms/Button",
+    components: AtomButton,
+    argTypes: {
+        label: {
+            control: 'text'
+        },
+        disabled: {
+            type: 'boolean'
+        },
+        theme: {
+            type: "select",
+            options: ["primary", "secondary", "link", "negative", "underline", "underline-negative"]
+        },
+        size: {
+            type: "select",
+            options: ["sm", "button", "lg"]
+        },
+        onClick: {action: 'handleClick'}
+
+    },
+    decorators: [
+        () => ({
+            template:
+                "<div class=\"storybook-container\"><h2 class=\"storybook-title\">다중 항목 선택 시 사용합니다.</h2><div class=\"storybook-group\"><story /></div></div>"
+        })
+    ]
 };
 
-export const Default = () => ({
-  components: { AtomButton },
-  template: `<atom-button>
-	<span class="button__text">Button</span>
-  </atom-button>`
+const Template = (args) => ({
+    components: {AtomButton},
+    setup() {
+        return {args};
+    },
+    template:
+        `
+      <div class="v-group">
+        <div class="h-group">
+          <atom-button v-bind="args"></atom-button>
+        </div>       
+      </div>
+    `
 });
 
-export const Primary = () => ({
-    components: { AtomButton },
-    template: `<atom-button class="button--primary">
-    <span class="button__text">Button</span>
-    </atom-button>`
-});
-
-export const Secondary = () => ({
-    components: { AtomButton },
-    template: `<atom-button class="button--secondary">
-		<span class="button__text">Button</span>
-    </atom-button>`
-});
-
-export const Tertiary = () => ({
-    components: { AtomButton },
-    template: `<atom-button class="button--link">
-		<span class="button__text">Button</span>
-    </atom-button>`
-});
+export const Default = Template.bind({});
--- frontend/src/components/atoms/button/atom-button.vue
+++ frontend/src/components/atoms/button/atom-button.vue
@@ -1,11 +1,16 @@
 <template>
 	<!-- type="submit" 인 경우, id가 필요 -->
-	<button class="button" type="submit" :id="submitId" v-if="submitId">
-		<slot></slot>
+	<button class="button" type="submit" :class="[themeClass, sizeClass]" :id="submitId" v-if="submitId"
+					:disabled="disabled" :title="title">
+		<slot>{{ label }}</slot>
 	</button>
 	<!-- 그 외 타입은 id 불필요 -->
-	<button class="button" type="button" @click="handleClick($event)" @mousedown="mouseDown($event)" v-else>
-		<slot></slot>
+	<button class="button" type="button" :class="[themeClass, sizeClass]"
+					:disabled="disabled" :title="title"
+					@click="handleClick($event)"
+					@mousedown="mouseDown($event)"
+					v-else>
+		<slot>{{ label }}</slot>
 	</button>
 </template>
 
@@ -16,11 +21,40 @@ export default {
 		submitId: {
 			type: String,
 			default: null
+		},
+		label: {
+			type: String,
+			default: "Button"
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		},
+		title: {
+			type: String,
+			default: "button"
+		},
+		theme: {
+			type: String,
+			theme: ""
+		},
+		size: {
+			type: String,
+			default: ""
+		}
+
+	},
+	computed: {
+		themeClass() {
+			return !_.isEmpty(this.theme) ? `button--${this.theme}` : "";
+		},
+		sizeClass() {
+			return !_.isEmpty(this.size) ? `button--${this.size}` : "";
 		}
 	},
-	computed: {},
 	methods: {
 		handleClick($event) {
+			console.log('Clicked');
 			this.$emit("click", $event);
 		},
 		mouseDown($event) {
